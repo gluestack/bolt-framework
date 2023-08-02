1a589b970b239c9a78c083cad8e1d4dc474ae13d
Tanmay Jaiswal <tanmayjaiswal2201@gmail.com>
2023-08-02 19:58:07 +0530

    chore: removed unnecessary import

--- packages/bolt/src/actions/route-generate.ts
+++ packages/bolt/src/actions/route-generate.ts
@@ -1,11 +1,8 @@
-import chalk from "chalk";
 import Common from "../common";
-import { execute } from "../helpers/execute";
 
 import generateRoutes from "../helpers/generate-routes";
 import { validateMetadata } from "../helpers/validate-metadata";
 import { validateServices } from "../helpers/validate-services";
-import EnvGenerate from "./env-generate";
 
 export default class RouteGenerate {
   public async handle(isProd: boolean = false) {
